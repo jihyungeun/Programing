fad7b6ff608a665ead5654099f82bf28f3d7dc42
HyeongeunJi <jihk0909@gmail.com>
2021-04-13 22:16:26 +0900

    Create 05.c

---
+++ C/C_Primier_Plus_6/02/practice/05.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void br(){
+	printf("Brazil, Russia");
+}
+
+void ic(){
+	printf("India, China");
+}
+
+int main(){
+	br();
+	printf(", ");
+	ic();
+	printf("\n");
+	ic();
+	printf(",\n");
+	br();
+	printf("\n");
+	return 0;
+}
