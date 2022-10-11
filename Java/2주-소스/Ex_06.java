209848f09c5dc1a1623b5e6e555902d48e85d556
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:14:58 +0900

    Create Ex_06.java

---
+++ Java/2주-소스/Ex_06.java
@@ -0,0 +1,11 @@
+// 정상적인 자료 처리
+public class Ex_06 {
+	public static void main(String[] ar){
+		int x = 128;
+		int y = x;
+				
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+		
+	}
+}
