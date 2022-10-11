777b747ab365c363e3ec0ca0fba4cb96efad0082
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:13:13 +0900

    Create Ex_02.java

---
+++ Java/1주-소스/Ex_02.java
@@ -0,0 +1,9 @@
+public class Ex_02 {
+	public static void main(String[] ar){
+		int a, b, sum;
+		a = Integer.parseInt(ar[0]);
+		b = Integer.parseInt(ar[1]);
+		sum = a + b;
+		System.out.println("두 수의 합은 " + sum + " 입니다");
+	}
+}
