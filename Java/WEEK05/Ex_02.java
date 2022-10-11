f55e3c8483375da83552bdc71a7829b680b69ba4
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:16:58 +0900

    Create Ex_02.java

---
+++ Java/WEEK05/Ex_02.java
@@ -0,0 +1,21 @@
+
+public class Ex_02 {
+	
+	public static void main(String args[]) {
+	    
+	    System.out.println("* 구구단 3단 *");
+	    
+	    int n = 1;
+	    while( n < 10){ 
+	    	System.out.println(" " + 3 + "*" + n + " = " + (3*n));
+	        n++;
+	    } 
+	 }
+}
+	/* 
+	do-while
+		int n = 1;
+	    do { System.out.println("   " + 3 + "*" + n + " = " + (3*n));
+	            n++;
+	    } while(n < 10);
+	*/
