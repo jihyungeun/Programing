2d1639bd3020c337b1f1204bf00366d8f39d4ed4
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:13:26 +0900

    Create Ex_03.java

---
+++ Java/1주-소스/Ex_03.java
@@ -0,0 +1,11 @@
+//public class 이름과 파일이름 같아야한다
+/* class는 {}*/
+/** " " 화면상에 나타나는 문구*/
+// 맨 끝에 세미콜론 반드시 붙일 것
+public class Ex_03 {
+	public static void main(String[] ar){
+		//String ar[] or String args[]
+		System.out.println("자바의 세상에 오신것을 환영합니다");
+		System.out.println("Yang Gye Tak");
+	}
+}
