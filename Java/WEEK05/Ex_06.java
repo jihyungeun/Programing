3ceaabd1cb5e6d2bf948e6955042cdce6c2a3f8d
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:17:12 +0900

    Create Ex_06.java

---
+++ Java/WEEK05/Ex_06.java
@@ -0,0 +1,15 @@
+// for문 조건 변화
+public class Ex_06 {
+	
+	public static void main(String args[]) {
+		System.out.println("1부터 7까지의 정수");
+		
+			for(int i = 1; i <= 7 ; i++){
+    	       System.out.println(i);	    	
+           }
+	}
+}
+/*
+for(int i = 7; i >= 1 ; i--){
+    System.out.println("i");	    	
+*/
