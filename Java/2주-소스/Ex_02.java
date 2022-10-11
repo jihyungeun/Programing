5edd19beeb037ab6f5ca079fc84c0d60165fadad
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:13:36 +0900

    Create Ex_02.java

---
+++ Java/2주-소스/Ex_02.java
@@ -0,0 +1,23 @@
+//class, method, field, 변수 : 이름 붙이는 방법
+public class Ex_02 {
+	private String name;	private String _aa;
+	private String $bb;		private String c_c;
+	private String assasasasdasda;
+	//private String aa bbb;
+	//private String *ab;
+	//private String aa&bb;
+	//private String 1ab;
+	private String a1b;
+	//private String for;
+	
+	public Ex_02(){}
+	public void display() {}
+	// class의 이름 첫 글자 대문자	
+	// method 첫자는 소문자, 메소드는 ()  
+	// Exam_02는 생성자
+}
+// 식별자는 문자, 숫자, 특수문자(_, $)로 구성
+// 식별자의 첫 문자는 문자나 특수문자로 시작(숫자는 안됨)
+// 예약어, true, false, null 는 식별자로 사용할 수 없음
+// 식별자는 길이에 제한을 두지 않음
+// 대소문자(Abc과 abc)는 서로 다른 식별자로 취급 
