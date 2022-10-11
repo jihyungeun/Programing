a6261b84dfd711d10eee3caa5a308878ec6a9d98
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:16:02 +0900

    Create Ex_02.java

---
+++ Java/WEEK04/Ex_02.java
@@ -0,0 +1,15 @@
+// String
+public class Ex_02{
+	
+	public static void main(String args[]){
+		
+		String str1 = "아! 대한민국 ";
+		String str2 = new String("Korea");
+		System.out.println(str1 + str2);
+		
+		int a = 1000;
+		int b = 2000;
+		System.out.println(str1 + a + b + "리 금수강산 ");
+		System.out.println(str1 + (a + b) + "리 금수강산 ");
+	}
+}
