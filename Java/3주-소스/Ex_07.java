fd1723939b93aaf306b66c0837cb8db5c222d29b
jihyungeun <jihk0909@gmail.com>
2022-10-12 05:15:46 +0900

    Create Ex_07.java

---
+++ Java/3주-소스/Ex_07.java
@@ -0,0 +1,16 @@
+public class Ex_07 {
+	public static void main(String args[]){
+		int a = 10;
+		System.out.println("a = " + a);
+		a += 4;
+		System.out.println("a += 4의 결과 " + a);
+		a -= 4;
+		System.out.println("a -= 4의 결과 " + a);
+		a *= 4;
+		System.out.println("a *= 4의 결과 " + a);
+		a /= 4;
+		System.out.println("a /= 4의 결과 " + a);
+		a %= 4;
+		System.out.println("a %= 4의 결과 " + a);
+	}
+}
