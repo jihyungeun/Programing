b74cd66511bbd13749a1ef80b00b4ef1c32db90a
jihyngeun <jihk0909@gmail.com>
2022-10-14 14:03:11 +0900

    Create ex_12.py

---
+++ Python/전공/10-14/ex_12.py
@@ -0,0 +1,11 @@
+import sys
+print(sys.argv)
+print("---")
+
+print("getwindowsversion:()", sys.getwindowsversion())
+print("---")
+print("copyright:", sys.copyright)
+print("---")
+print("verison:", sys.version)
+
+sys.exit()
