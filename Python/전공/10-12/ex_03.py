6b7718c4c868d6eec20d33aef8944bf4bcde2610
jihyngeun <jihk0909@gmail.com>
2022-10-14 13:33:51 +0900

    Create ex_03.py

---
+++ Python/전공/10-12/ex_03.py
@@ -0,0 +1,6 @@
+#06_01_6 3강
+number_input_a = int(input("정수입력> "))
+
+print("원의 반지름:", number_input_a)
+print("원의 둘레:", 2 * 3.14 * number_input_a)
+print("원의 넓이:", 3.15 * number_input_a * number_input_a)
